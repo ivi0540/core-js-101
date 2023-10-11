f821a97d4a646c6136685b8b106689a0b9c38e13
ivi0540 <ivi0540@gmail.com>
2023-10-11 11:42:23 +0300

    feat: save9

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -68,8 +68,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.slice(7, value.length - 1);
+  // throw new Error('Not implemented');
 }
 
 
