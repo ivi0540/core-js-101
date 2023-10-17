dc22ee65ec867cac2eb659082241811c896450a3
ivi0540 <ivi0540@gmail.com>
2023-10-17 22:57:11 +0300

    feat: save55

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -195,8 +195,10 @@ function getHead(arr, n) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  let newArr = arr.reverse();
+  newArr = newArr.slice(0, n);
+  return newArr.reverse();
 }
 
 
