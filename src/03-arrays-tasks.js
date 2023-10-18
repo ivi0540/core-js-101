62d92a5ba179306d4711f176299d030dcf5bcdc5
ivi0540 <ivi0540@gmail.com>
2023-10-18 11:19:26 +0300

    feat: save56

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -222,8 +222,9 @@ function getTail(arr, n) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText(arr) {
+  const result = arr.map((elem) => elem.join(','));
+  return result.join('\n');
 }
 
 /**
