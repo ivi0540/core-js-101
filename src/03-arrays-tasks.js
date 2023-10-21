1f6865a581f6aff656890264b61901584f485ecd
ivi0540 <ivi0540@gmail.com>
2023-10-21 20:15:42 +0300

    feat: save69

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -542,8 +542,9 @@ function findAllOccurrences(arr, item) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  const result = arr.map((elem) => String(elem));
+  return result.join(',');
 }
 
 
