c33e5d69d409dac77050fbac8d7988b1e17c6fb4
ivi0540 <ivi0540@gmail.com>
2023-10-21 12:33:37 +0300

    feat: save65

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -406,8 +406,69 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  function nameToNumber(nameArr) {
+    function forReturnNameToNumber(elem2) {
+      switch (elem2) {
+        case 'zero':
+          return 0;
+        case 'one':
+          return 1;
+        case 'two':
+          return 2;
+        case 'three':
+          return 3;
+        case 'four':
+          return 4;
+        case 'five':
+          return 5;
+        case 'six':
+          return 6;
+        case 'seven':
+          return 7;
+        case 'eight':
+          return 8;
+        case 'nine':
+          return 9;
+        default:
+          return null;
+      }
+    }
+    const result = nameArr.map((elem) => forReturnNameToNumber(elem));
+    return result;
+  }
+  function numberToName(nameArr) {
+    function forReturnNumberToName(elem2) {
+      switch (elem2) {
+        case 0:
+          return 'zero';
+        case 1:
+          return 'one';
+        case 2:
+          return 'two';
+        case 3:
+          return 'three';
+        case 4:
+          return 'four';
+        case 5:
+          return 'five';
+        case 6:
+          return 'six';
+        case 7:
+          return 'seven';
+        case 8:
+          return 'eight';
+        case 9:
+          return 'nine';
+        default:
+          return null;
+      }
+    }
+    const result = nameArr.map((elem) => forReturnNumberToName(elem));
+    return result;
+  }
+  const result = nameToNumber(arr);
+  return numberToName(result.sort((a, b) => a - b));
 }
 
 /**
