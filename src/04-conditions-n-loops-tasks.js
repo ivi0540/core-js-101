14400a91da07a22c0a6db557293645cb9122b2f0
ivi0540 <ivi0540@gmail.com>
2023-10-24 11:11:24 +0300

    feat: save78

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -46,8 +46,12 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let work = 1;
+  for (let i = 1; i <= n; i += 1) {
+    work *= i;
+  }
+  return work;
 }
 
 
