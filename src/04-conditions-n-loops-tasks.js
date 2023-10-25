bf2b191d6dbdff94dbf11199df0c35a1bb99576c
ivi0540 <ivi0540@gmail.com>
2023-10-25 12:42:19 +0300

    feat: save88

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -316,8 +316,16 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  function sumElemNum(num2) {
+    const newNum = String(num2);
+    return newNum.reduce((sum, elem) => sum + Number(elem), 0);
+  }
+
+  if (sumElemNum(num) > 9) { return sumElemNum(sumElemNum(num)); }
+  if (sumElemNum(num) < 9) { return sumElemNum(num); }
+
+  return null;
 }
 
 
